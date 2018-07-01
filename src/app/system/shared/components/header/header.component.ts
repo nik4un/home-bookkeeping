8014c426f769406caacef6b544f28b5e15e3a699
Nik <nik@iMac.local>
2018-07-01 18:44:44 +0300

    Создание header

---
+++ src/app/system/shared/components/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../../shared/services/auth.service';
+import { User } from '../../../../shared/models/user.model';
+
+@Component({
+  selector: 'hb-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  date: Date = new Date();
+  user: User;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+) { }
+
+  ngOnInit() {
+    this.user = JSON.parse(window.localStorage.getItem('user'));
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
