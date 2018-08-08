3661c6c3c450af93a6ac7c9d2b37662b837f336f
nik4un <nik4un@yandex.ru>
2018-08-08 11:31:12 +0300

    Страница 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 // необходимо создать массив роутов, которве будут корневыми для всего приложения
 const routes: Routes = [
   { path: '', component: AuthComponent},
-  { path: 'system', loadChildren: './system/system.module#SystemModule' }
+  { path: 'system', loadChildren: './system/system.module#SystemModule' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   imports: [ReactiveFormsModule, FormsModule, NgxChartsModule],
-  exports: [ReactiveFormsModule, FormsModule, NgxChartsModule]
+  exports: [ReactiveFormsModule, FormsModule, NgxChartsModule],
+  declarations: []
 })
 export class SharedModule {}
