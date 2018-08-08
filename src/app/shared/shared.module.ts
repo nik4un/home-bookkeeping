23c2c38aa396fa7bf298a9a9b53f1853385de3c7
nik4un <nik4un@yandex.ru>
2018-08-08 12:52:11 +0300

    Красивый loader

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
   imports: [ReactiveFormsModule, FormsModule, NgxChartsModule],
-  exports: [ReactiveFormsModule, FormsModule, NgxChartsModule],
-  declarations: []
+  exports: [ReactiveFormsModule, FormsModule, NgxChartsModule, LoaderComponent],
+  declarations: [LoaderComponent]
 })
 export class SharedModule {}
